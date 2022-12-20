9853dce54605b44d30b934c2f7e7dad5ab68ac73
Arya Karimi <devaryakrm@gmail.com>
2022-12-20 21:29:22 +0300

    Exercise Updated and finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,6 @@
 import React, { useState } from 'react';
 import { nanoid } from 'nanoid';
 
-// const PLACE = {
-//     id: nanoid(),
-//     name: PlaceName,
-//     items: [],
-//     image: '',
-// };
-
-1;
-
-2;
-
-3;
-
-4;
-
-// const ITEM = {
-//     id: nanoid(),
-//     name: '',
-// };
-
 const animal_init = {
     id: nanoid(),
     type: null, // display this
@@ -36,11 +16,6 @@ const animal_init = {
     isHere: true,
 };
 
-//<form> with one input field (Animal's type) and one button (save Animal type)
-//<button> that says either Go on a Journey or Return from the Journey (depends on
-//isHere)
-//
-//
 const App = () => {
     const [input, setInput] = useState('');
     const [animal, setAnimal] = useState(animal_init);
@@ -56,6 +31,20 @@ const App = () => {
         name,
     }));
 
+    const DESERT = {
+        id: nanoid(),
+        name: 'desert',
+        items: DESERT_ITEMS,
+        image: 'https://cdn.pixabay.com/photo/2016/09/08/13/58/desert-1654439_1280.jpg',
+    };
+
+    const LAKE = {
+        id: nanoid(),
+        name: 'lake',
+        items: LAKE_ITEMS,
+        image: 'https://cdn.pixabay.com/photo/2017/05/09/03/46/alberta-2297204_1280.jpg',
+    };
+
     const handleInput = (event) => {
         setInput(event.target.value);
     };
@@ -69,69 +58,94 @@ const App = () => {
         setInput('');
     };
 
+    const bringItems = (items, value, isHere) => {
+        if (value === 'lake' && !isHere)
+            return [...items, LAKE.items[Math.floor(Math.random() * 3)]]; // 3 is LAKE_ITEMS len
+        if (value === 'desert' && !isHere)
+            return [...items, DESERT.items[Math.floor(Math.random() * 3)]];
+        else return [...items];
+    };
+
     const toggleisHere = () => {
-        const { isHere } = animal;
-        console.log(placeValue);
-        if (placeValue === 'lake') {
-            setAnimal((oldSpecs) => ({
-                ...oldSpecs,
-                isHere: !isHere,
-                journeys: {
-                    ...journeys,
-                    lake: {
-                        ...journeys.lake,
-                        no: isHere ? journeys.lake.no + 1 : journeys.lake.no,
-                    },
-                },
-                items: [],
-            }));
-        }
-        if (placeValue === 'desert') {
-            setAnimal((oldSpecs) => ({
-                ...oldSpecs,
-                isHere: !isHere,
-                journeys: {
-                    ...journeys,
-                    desert: {
-                        ...journeys.desert,
-                        no: isHere
-                            ? journeys.desert.no + 1
-                            : journeys.desert.no,
-                    },
-                },
-                items: [],
-            }));
-        }
+        const { isHere, items } = animal;
+
+        setAnimal((oldSpecs) => ({
+            ...oldSpecs,
+            isHere: !isHere,
+            journeys: isHere
+                ? { ...journeys }
+                : {
+                      ...journeys,
+                      lake: {
+                          ...journeys.lake,
+                          no:
+                              placeValue === 'lake'
+                                  ? journeys.lake.no + 1
+                                  : journeys.lake.no,
+                      },
+                      desert: {
+                          ...journeys.desert,
+                          no:
+                              placeValue === 'desert'
+                                  ? journeys.desert.no + 1
+                                  : journeys.desert.no,
+                      },
+                  },
+            items: [...bringItems(items, placeValue, isHere)],
+        }));
     };
 
     const { id, isHere, type, journeys, items } = animal;
     console.log(isHere);
     return (
         <div>
-            <input type="text" value={input} onChange={handleInput} />
-            <button onClick={saveAnimalType}>save Animal Type</button>
-            <select value={placeValue} onChange={changePlace}>
-                <option value="">Choose One Place</option>
-                <option value="lake">Lake</option>
-                <option value="desert">Desert</option>
-            </select>
-            <button onClick={toggleisHere}>
-                {isHere ? 'Go To Journey' : 'Come Back from Journey'}
-            </button>
+            <div>
+                <input type="text" value={input} onChange={handleInput} />
+                <button onClick={saveAnimalType}>save Animal Type</button>
+                <select
+                    value={placeValue}
+                    onChange={changePlace}
+                    disabled={!isHere}
+                >
+                    <option value="">Choose One Place</option>
+                    <option value="lake">Lake</option>
+                    <option value="desert">Desert</option>
+                </select>
+            </div>
+            <div>
+                <button onClick={toggleisHere}>
+                    {isHere ? 'Go To Journey' : 'Come Back from Journey'}
+                </button>
+                {!isHere && (
+                    <img
+                        src={placeValue === 'lake' ? LAKE.image : DESERT.image}
+                        alt=""
+                    />
+                )}
+            </div>
             <ul>
                 <li>Animal id: {id}</li>
-                <li>
-                    Total Journies Number:{' '}
-                    {journeys.lake.no + journeys.desert.no}
-                </li>
                 <li>Animal Type: {type}</li>
                 <li>
                     Animal Status:{' '}
-                    {isHere ? 'Go to Journey' : 'Return from Journey'}
+                    {isHere ? 'The Animal is here!' : 'The Animal is gone!'}
                 </li>
-                <li>Journies To lake: {journeys.lake.no}</li>
-                <li>Journies To desert: {journeys.desert.no}</li>
-                <li>Animal items: {items}</li>
+                <li>
+                    Total Journeys Number:{' '}
+                    {journeys.lake.no + journeys.desert.no}
+                </li>
+                <li>Journeys To lake: {journeys.lake.no}</li>
+                <li>Journeys To desert: {journeys.desert.no}</li>
+                <li>
+                    Animal items:{' '}
+                    {items.map((item) => {
+                        return (
+                            <div key={item.id}>
+                                <p>{item.name}</p>
+                            </div>
+                        );
+                    })}
+                </li>
             </ul>
         </div>
     );
